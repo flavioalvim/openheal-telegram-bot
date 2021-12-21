774536116e287229b5c94ae061fa9dbba956009f
flavioalvim <flavioalvim@gmail.com>
2021-12-21 10:06:41 -0300

    terminando dutySacleService

--- bot/middlewares/scenes/dutyScaleSceneMiddleware.js
+++ bot/middlewares/scenes/dutyScaleSceneMiddleware.js
@@ -1,35 +1,15 @@
 const Scene = require ('telegraf/scenes/base')
 const Stage = require ('telegraf/stage')
 const {enter, leave} = Stage
-const {KeyboardFromArray, Keyboard} = require ('../../components/keyboard')
 const fs = require('fs')
-const {getDataBase, getSpecialties,getScaleEchoKeyboardObject, getScaleOrthopedicsKeyboardObject} = require ('../../services/dutyScaleService')
-
+const {
+    getDataBase, 
+    getExtraCommandsAndActionsObject,
+    getCorrectKeyboard,getMainScene} = require ('../../services/dutyScaleService')
 
 const dutyScaleScene = new Scene('dutyScaleScene') //Scene
-const db = getDataBase()
-const specialties = getSpecialties()
-const buttons = [...specialties, "sair"] // Array of Objects
 
-const regularKeyboard = new KeyboardFromArray(buttons) //Keboard
-const returnKeyboard = new KeyboardFromArray(["Voltar"])
-const scaleEchoKeyboard = new Keyboard(getScaleEchoKeyboardObject(), 1)
-const scaleOrthopedicsKeyboard = new KeyboardFromArray(getScaleOrthopedicsKeyboardObject(), 1)
-
-
-const keyboarOptions  = {
-    "ecocardiograma" : scaleEchoKeyboard,
-    "ortopedia" : scaleOrthopedicsKeyboard
-}
-
-const getCorrectKeyboard = (filter) => keyboarOptions[filter]?keyboarOptions[filter]:null
-const getSpecialtiesScales = () => specialties.reduce((acc,item)=>(`${acc} \n /${item}`),"Essas são as escalas de sobreaviso:") //String
-const getScaleText = (specialty) => db
-    .filter(item => item.specialty == specialty)
-    .map (item => item.scaleText).toString() // String de objetos filtrados
-
-    
-const regularCommandsAndActions = db.map(({specialty, professionals}) =>({
+const regularCommandsAndActions = getDataBase().map(({specialty, professionals}) =>({
     command: specialty,
     action :specialty,
     callBack: (ctx)=>{
@@ -37,48 +17,21 @@ const regularCommandsAndActions = db.map(({specialty, professionals}) =>({
         ctx.reply(telephonesText, getCorrectKeyboard(specialty))}
 })) //Array of Objects
 
-const extraCommandsAndActions = [
-    {
-        command: "sair",
-        action : "sair",
-        callBack : leave()
-    },
-    {
-        command: "Voltar",
-        action : "Voltar",
-        callBack: (ctx)=>ctx.reply("Voltar")
-    },
-    {
-        command: "Ver escala ecocardiograma",
-        action: "Ver escala ecocardiograma",
-        callBack: (ctx)=>ctx.reply(getScaleText("ecocardiograma"),returnKeyboard)
-    },
-    {
-        command: "Ver escala ortopedia",
-        action: "Ver escala ecocardiograma",
-        callBack: (ctx)=>ctx.reply(getScaleText("ortopedia"),returnKeyboard)
-    },
-    {
-        command: "Mão",
-        action: "Mão",
-        callBack: (ctx)=>ctx.reply("Aqui vai a mao",returnKeyboard)
-    }
-] // Array of objects
+const extraCommandsAndActions = getExtraCommandsAndActionsObject()
     
-    const commandsAndActions = [...regularCommandsAndActions, ...extraCommandsAndActions]
+const commandsAndActions = [...regularCommandsAndActions, ...extraCommandsAndActions]
 
 
-dutyScaleScene.enter(ctx=>ctx.reply(getSpecialtiesScales(), regularKeyboard))
+dutyScaleScene.enter(getMainScene)
 dutyScaleScene.leave(ctx=>ctx.reply("Saindo do mudulo escala. Digite algo para novas opções"))
 
-commandsAndActions.forEach(({command, callBack})=>{
-    dutyScaleScene.command(command,callBack)
-})
-dutyScaleScene.on('message', ctx=>ctx.reply(getSpecialtiesScales(),regularKeyboard))
 
-commandsAndActions.forEach(({action, callBack})=> {
+commandsAndActions.forEach(({action , command, callBack})=>{
+    dutyScaleScene.command(command,callBack)
     dutyScaleScene.action(action,callBack)
 })
+
+dutyScaleScene.on('message', getMainScene)
 
 
 module.exports = {dutyScaleScene}
--- bot/services/dutyScaleService.js
+++ bot/services/dutyScaleService.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
+const Stage = require ('telegraf/stage')
+const {enter, leave} = Stage
+const {KeyboardFromArray, Keyboard} = require ('../components/keyboard')
 
 const getDataBase = () => JSON.parse(fs.readFileSync ('../scales.json')) //Json
 const db = getDataBase()
 const getSpecialties = () => db.map(item => item.specialty) //Array of Objects
+
+const getSpecialtiesScales = () => getSpecialties().reduce((acc,item)=>(`${acc} \n /${item}`),"Essas são as escalas de sobreaviso:") //String
 
 const getScaleEchoKeyboardObject = ()=>
 [
@@ -15,16 +20,76 @@ const getScaleEchoKeyboardObject = ()=>
         action: "Voltar"
     }]
 
-//const getScaleOrthopedicsKeyboardObject = () => [ "Mão", "Ombro e cotovelo", "Quadril", "Joelho", "Pé e tornozelo", "Coluna", "Politrauma e diáfises"]
 const getScaleOrthopedicsKeyboardObject = () => {
     return [ "Mão", "Ombro e cotovelo", "Quadril", "Joelho", "Pé e tornozelo", "Coluna", "Politrauma e diáfises"]
 }
 
 
+
+const buttons = [...getSpecialties(), "sair"] // Array of Objects
+const regularKeyboard = new KeyboardFromArray(buttons) //Keboard
+const returnKeyboard = new KeyboardFromArray(["Voltar"])
+const scaleEchoKeyboard = new Keyboard(getScaleEchoKeyboardObject(), 1)
+const scaleOrthopedicsKeyboard = new KeyboardFromArray(getScaleOrthopedicsKeyboardObject(), 1)
+
+const keyboarOptions  = {
+    "ecocardiograma" : scaleEchoKeyboard,
+    "ortopedia" : scaleOrthopedicsKeyboard,
+    "regular" : regularKeyboard,
+    "return" : returnKeyboard
+}
+
+const getCorrectKeyboard = (filter) => keyboarOptions[filter]?keyboarOptions[filter]:null
+
+
+const getExtraCommandsAndActionsObject = ()=>
+[
+    {
+        command: "sair",
+        action : "sair",
+        callBack : leave()
+    },
+    {
+        command: "Voltar",
+        action : "Voltar",
+        callBack: getMainScene
+    },
+    {
+        command: "Ver escala ecocardiograma",
+        action: "Ver escala ecocardiograma",
+        callBack: (ctx)=>ctx.reply(getScaleText("ecocardiograma"),getCorrectKeyboard("return"))
+    },
+    {
+        command: "Ver escala ortopedia",
+        action: "Ver escala ecocardiograma",
+        callBack: (ctx)=>ctx.reply(getScaleText("ortopedia"),getCorrectKeyboard("return"))
+    },
+    {
+        command: "Mão",
+        action: "Mão",
+        callBack: (ctx)=>ctx.reply("Aqui vai a mao",returnKeyboard)
+    }
+] // Array of objects
+
+
+const getMainScene = ctx => ctx.reply(getSpecialtiesScales(), getCorrectKeyboard("regular"))
+
+const getScaleText = (specialty) => getDataBase()
+    .filter(item => item.specialty == specialty)
+    .map (item => item.scaleText).toString() // String de objetos filtrados
+
+
+
+
+//const getScaleOrthopedicsKeyboardObject = () => [ "Mão", "Ombro e cotovelo", "Quadril", "Joelho", "Pé e tornozelo", "Coluna", "Politrauma e diáfises"]
+
+
+
+
+
 module.exports = { 
     getDataBase,
-    getSpecialties,
-    getScaleEchoKeyboardObject,
-    getScaleOrthopedicsKeyboardObject
-
+    getExtraCommandsAndActionsObject,
+    getCorrectKeyboard,
+    getMainScene,
 }
