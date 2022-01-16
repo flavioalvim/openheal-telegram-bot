d8d0f41378436b31e1b8e00e9383f7afb7526c4e
flavioalvim <flavioalvim@gmail.com>
2022-01-16 00:31:36 -0300

    correcao do nome protocolos

--- bot/middlewares/scenes/protocols.js
+++ bot/middlewares/scenes/protocols.js
@@ -1,8 +1,8 @@
 const Scene = require('telegraf/scenes/base')
-const getCommandsAndActionsObject  = require('../../services/protocolosServices/object')
-//const getCorrectKeyboard = require('../../services/protocolosServices/keyboards')
+const getCommandsAndActionsObject  = require('../../services/protocolsServices/object')
+//const getCorrectKeyboard = require('../../services/protocolsServices/keyboards')
 const protocolsScene = new Scene('protocolsScene')
-const getMainScene = require('../../services/protocolosServices/callbacks')
+const getMainScene = require('../../services/protocolsServices/callbacks')
 const  {mainMenuOptions} =require('../../components/mainMenu')
 
 const commandsAndActions = getCommandsAndActionsObject()
