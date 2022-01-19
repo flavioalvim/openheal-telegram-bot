d313939b1311e0697556b4a9c5f33916aea8ab7a
flavioalvim <flavioalvim@gmail.com>
2022-01-18 21:30:03 -0300

    Corrigido erro de merge

--- bot/bot.js
+++ bot/bot.js
@@ -5,21 +5,13 @@ const session = require('telegraf/session')
 // const { KeyboardFromArray } = require('./components/keyboard')
 const {
     dutyScaleScene,
-<<<<<<< Updated upstream
-    protocolosCovidScene,
-} = require('./middlewares')
-const verifyUserMiddleware = require('./middlewares/verifyUserMiddleware')
-const {
-    getUserContactMiddleware,
-} = require('./middlewares/getUserContactMiddleware')
-=======
     protocolsScene
 
 } = require('./middlewares')
 const verifyUserMiddleware = require('./middlewares/verifyUserMiddleware')
 const {getUserContactMiddleware} = require('./middlewares/getUserContactMiddleware')
 const {commands,actions,mainMenuOptions} = require('./components/mainMenu')
->>>>>>> Stashed changes
+
 
 const { NODE_ENV = 'development', TELEGRAM_TOKEN } = process.env
 
@@ -31,53 +23,18 @@ if (NODE_ENV === 'production') {
 }
 
 const bot = new Telegraf(TELEGRAM_TOKEN)
-<<<<<<< Updated upstream
-const { enter } = Stage
-const k = new KeyboardFromArray(['escala', 'protocolos_covid'], 1)
-//const k1 = new KeyboardFromArray(['escala', 'voltar'], 3)
-
-const stage = new Stage([dutyScaleScene, protocolosCovidScene])
-=======
-
-
 const stage = new Stage([dutyScaleScene,protocolsScene])
->>>>>>> Stashed changes
+
 bot.use(session())
 bot.use(stage.middleware())
 bot.use(getUserContactMiddleware)
 bot.use(verifyUserMiddleware)
 
-<<<<<<< Updated upstream
-const commands = [
-    {
-        command: 'escalas',
-        callBack: enter('dutyScaleScene'),
-    },
-    {
-        command: 'protocolos_covid',
-        callBack: enter('protocolosCovidScene'),
-    },
-]
-=======
->>>>>>> Stashed changes
 
 commands.forEach(({ command, callBack }) => {
     bot.command(command, callBack)
 })
 
-<<<<<<< Updated upstream
-const actions = [
-    {
-        action: 'escala',
-        callback: enter('dutyScaleScene'),
-    },
-    {
-        action: 'protocolos_covid',
-        callback: enter('protocolosCovidScene'),
-    },
-]
-=======
->>>>>>> Stashed changes
 
 actions.forEach(({ action, callback }) => {
     bot.action(action, callback)
