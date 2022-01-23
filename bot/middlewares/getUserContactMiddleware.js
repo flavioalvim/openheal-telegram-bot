a1f9ce5c951227499b47c6cb6642f2e008fe4dc4
flavioalvim <flavioalvim@gmail.com>
2022-01-23 15:00:02 -0300

    alteracoes em getUserId

--- bot/middlewares/getUserContactMiddleware.js
+++ bot/middlewares/getUserContactMiddleware.js
@@ -1,13 +1,23 @@
-
+require('dotenv').config()
 const fs = require('fs')
+const Telegram = require('telegraf/telegram')
+
+const {
+    TELEGRAM_TOKEN
+} = process.env
+
 
 let data = []
 
 const getUserContactMiddleware = (ctx, next) => {
-    if(ctx.update.message){
-    data.push(ctx.update.message.from)}
+    if(ctx.update.callback_query){
+    console.log(`${ctx.update.callback_query.from.first_name} - ${ctx.update.callback_query.from.id}`)
+    const telegram = new Telegram("1869286697:AAHhrYfBJHaWJkwUZ-diDP65fGRAu6_uHAo")
+    telegram.sendMessage(1846365371, `${ctx.update.callback_query.from.first_name} - ${ctx.update.callback_query.from.id}`)
+}
     next()
 }
+
 
 module.exports = {
     getUserContactMiddleware,
